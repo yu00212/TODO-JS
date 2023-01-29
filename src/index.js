1d228eb8f3159fc1204857498a2a7386c07bdc80
ゆう <yu2000212@icloud.com>
2023-01-29 02:16:18 +0000

    削除機能

--- src/index.js
+++ src/index.js
@@ -18,10 +18,18 @@ const onClickAdd = () => {
   // button（完了）タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  })
   
   // button（削除）タグ生成
   const deleteButton = document.createElement("button");
-  completeButton.innerText = "削除";
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget =  deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
